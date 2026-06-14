dc8969332fb4265a406dcdf458e08d6807543b06
lbsantos1401-cloud <lbsantos1401@gmail.com>
2026-06-14 04:50:42 -0300

    main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+let moveForward = false;
+let turnLeft = false;
+let turnRight = false;
+
+
 import * as THREE from
 'https://unpkg.com/three@0.166.1/build/three.module.js';
 
@@ -13,6 +18,7 @@ let controller;
 let reticle;
 let hitTestSource = null;
 let hitTestSourceRequested = false;
+let placedCube = null;
 
 init();
 
@@ -137,16 +143,19 @@ function onSelect(){
 if(!reticle.visible)
 return;
 
-const car =
+if(placedCube)
+return;
+
+placedCube =
 new THREE.Group();
 
 const body =
 new THREE.Mesh(
 
 new THREE.BoxGeometry(
-0.08,
-0.04,
-0.12
+0.05,
+0.025,
+0.08
 ),
 
 new THREE.MeshStandardMaterial({
@@ -156,15 +165,17 @@ color:0xff3333
 );
 
 body.position.y =
-0.03;
+0.02;
 
-car.add(body);
+placedCube.add(body);
 
-car.position.setFromMatrixPosition(
+placedCube.position.setFromMatrixPosition(
 reticle.matrix
 );
 
-scene.add(car);
+scene.add(
+placedCube
+);
 
 }
 
@@ -208,6 +219,64 @@ source;
 
 }
 );
+
+const btnForward =
+document.getElementById(
+'forward'
+);
+
+const btnLeft =
+document.getElementById(
+'left'
+);
+
+const btnRight =
+document.getElementById(
+'right'
+);
+
+btnForward.addEventListener(
+'touchstart',
+()=>{
+moveForward = true;
+}
+);
+
+btnForward.addEventListener(
+'touchend',
+()=>{
+moveForward = false;
+}
+);
+
+btnLeft.addEventListener(
+'touchstart',
+()=>{
+turnLeft = true;
+}
+);
+
+btnLeft.addEventListener(
+'touchend',
+()=>{
+turnLeft = false;
+}
+);
+
+btnRight.addEventListener(
+'touchstart',
+()=>{
+turnRight = true;
+}
+);
+
+btnRight.addEventListener(
+'touchend',
+()=>{
+turnRight = false;
+}
+);
+
 
 }
 );
@@ -257,6 +326,32 @@ pose.transform.matrix
 );
 
 }
+
+}
+
+}
+
+if(placedCube){
+
+if(moveForward){
+
+placedCube.translateZ(
+-0.01
+);
+
+}
+
+if(turnLeft){
+
+placedCube.rotation.y +=
+0.03;
+
+}
+
+if(turnRight){
+
+placedCube.rotation.y -=
+0.03;
 
 }
 
