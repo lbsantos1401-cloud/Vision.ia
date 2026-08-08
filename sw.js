41c7bc801f715094deb704c13f1ecc1aa3cdcb99
lbsantos1401-cloud <lbsantos1401@gmail.com>
2026-08-08 03:24:51 -0300

    Clean up sw.js by removing comments
    Removed comments and documentation from sw.js.

--- sw.js
+++ sw.js
@@ -1,8 +1,4 @@
-/**
- * SOLTEC CLOUD - Service Worker para Notificações Push Movel
- */
-
-// Escuta o evento de exibição de notificação no celular
 self.addEventListener('notificationclick', (event) => {
   event.notification.close();
 });
+
